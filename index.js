580118ebf122c0c9666bc45889b1c5d1acb257a7
arishorts <arieljschwartz@gmail.com>
2022-12-27 19:23:17 -0700

    Refactored Index.js to look cleaner

--- index.js
+++ index.js
@@ -5,41 +5,28 @@ const btnEl4 = document.querySelector(".btn4");
 const btnEl5 = document.querySelector(".btn5");
 
 btnEl1.addEventListener("mouseover", (event) => {
-  const newposX = event.offsetX;
-  const newposY = event.offsetY;
-
-  document.querySelector("html").style.setProperty("--leftPos", newposX + "px");
-  document.querySelector("html").style.setProperty("--topPos", newposY + "px");
+  rippleevent(btnEl1);
 });
 
 btnEl2.addEventListener("mouseover", (event) => {
-  const newposX = event.offsetX;
-  const newposY = event.offsetY;
-
-  document.querySelector("html").style.setProperty("--leftPos", newposX + "px");
-  document.querySelector("html").style.setProperty("--topPos", newposY + "px");
+  rippleevent(btnEl2);
 });
 
 btnEl3.addEventListener("mouseover", (event) => {
-  const newposX = event.offsetX;
-  const newposY = event.offsetY;
-
-  document.querySelector("html").style.setProperty("--leftPos", newposX + "px");
-  document.querySelector("html").style.setProperty("--topPos", newposY + "px");
+  rippleevent(btnEl3);
 });
 
 btnEl4.addEventListener("mouseover", (event) => {
-  const newposX = event.offsetX;
-  const newposY = event.offsetY;
-
-  document.querySelector("html").style.setProperty("--leftPos", newposX + "px");
-  document.querySelector("html").style.setProperty("--topPos", newposY + "px");
+  rippleevent(btnEl4);
 });
 
 btnEl5.addEventListener("mouseover", (event) => {
+  rippleevent(btnEl5);
+});
+
+function rippleevent(btn) {
   const newposX = event.offsetX;
   const newposY = event.offsetY;
-
   document.querySelector("html").style.setProperty("--leftPos", newposX + "px");
   document.querySelector("html").style.setProperty("--topPos", newposY + "px");
-});
+}
